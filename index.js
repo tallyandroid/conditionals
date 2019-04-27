9105a5093ef055fa42cbf5b8dee3feb698de2858
Christian Tisby <christiantisby@Christians-MBP.hsd1.fl.comcast.net>
2019-04-27 02:22:04 -0400

    final commit

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ function getNum() {
 class NumPicker extends React.Component {
     render() {
         const num = getNum()
+        let msg;
+        if(num === 7) {
+            msg =
+           <div>
+           <h2>CONGRATS YOU WIN!!!</h2>
+               <img src="https://media.giphy.com/media/5Rj8JNS70TcL6/giphy.gif"/>
+           </div>
+        } else {
+            msg = <p>Sorry You Lose!</p>
+        }
         return ( 
         <div>
             <h1>Your number is {num}</h1>
-            <p>{num === 7 ? 'Congrats!': 'Unlucky!'}</p>
-            {
-                num === 7 && <img src="https://media.giphy.com/media/5Rj8JNS70TcL6/giphy.gif"/>
-                
-            }
+            {msg}
         </div>
         )
     }
